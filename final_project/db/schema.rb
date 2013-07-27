8d4fa3a12f2e7408c6528083cf6c0509b4e122e7
Brad Wilcox <brad_wilcox@me.com>
2013-07-26 20:05:09 -0400

    addng responsive css and html to content class

--- final_project/db/schema.rb
+++ final_project/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130726214941) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "interns", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
